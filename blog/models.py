c5791951f3ca93b472d646dda5429f273907b8d8
ladsunny <nicksharma@outlook.com>
2018-07-16 17:09:35 -0400

    added home.html to holdings, added bootstrap code, and home shows DB objects

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=300)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    tags = models.CharField(max_length=300)
--- holdings/views.py
+++ holdings/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import holding
 
 # Create your views here.
+def home(request):
+    myholdings = holding.objects
+    return render(request, 'holdings/home.html', {'myholdings':myholdings})
