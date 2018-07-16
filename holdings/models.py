893f748de8fed4477e8533a0fdf1dc6eb980ac17
ladsunny <nicksharma@outlook.com>
2018-07-15 21:06:55 -0400

    added models for holdings, created admin superuser, and migrated to Postgres

--- holdings/models.py
+++ holdings/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class holding(models.Model):
+    image = models.ImageField(upload_to='images/')
+    company = models.CharField(max_length=50)
+    weight = models.DecimalField(null=True,max_digits=5,decimal_places=2)
