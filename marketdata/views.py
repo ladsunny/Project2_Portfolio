2aeaf041ba8e82500e83a9939b406c7ddb5f2268
ladsunny <nicksharma@outlook.com>
2018-08-20 20:53:29 -0400

    fix in views.py (decode utf-8) for TypeError JSON object must be str

--- marketdata/views.py
+++ marketdata/views.py
@@ -10,10 +10,10 @@ def marketdata(request):
 
 	# Grab Stocks Price 2
 	stockprice_request = requests.get("https://api.iextrading.com/1.0/stock/market/batch?symbols=spy,amzn,googl,aapl,msft,fb,v,ma,baba,nflx,pypl,dis,gld,adbe,rht,unh,ba,brk.b,jpm,rtn,nvda,lmt,noc,sbux,atvi,tsm,asml&types=quote&range=1m&last=5")
-	stockprice = json.loads(stockprice_request.content)
+	stockprice = json.loads(stockprice_request.content.decode('utf-8'))
 
 	#Grab Most Active
 	mostactive_request = requests.get("https://api.iextrading.com/1.0/stock/market/list/mostactive")
-	mostactive = json.loads(mostactive_request.content)
+	mostactive = json.loads(mostactive_request.content.decode('utf-8'))
 
 	return render(request, 'marketdata.html', {'mostactive': mostactive, 'stockprice': stockprice, 'stocknews': stocknews})
